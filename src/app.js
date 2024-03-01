5eb8e7188304917253d4f0a93a6e709e4b26097d
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-03-02 00:07:11 +0530

    cors and cookieParser added

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import { Express } from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors" ; 
 
 
 const app = express()
@@ -13,7 +15,9 @@ app.use(cors({
 
 app.use(express.json({limit : "16kb"}))
 app.use(express.urlencoded({extended : true , limit : "16kb"}))
-app.use(express.sta)
+app.use(express.static("public"))
+
+app.use(cookieParser())
 
 
 export {app} ; 

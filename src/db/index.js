28825057af19f2891dd471f2201138fb3562f776
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-03-04 23:24:07 +0530

    solved minor errors

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import {DB_NAME} from "../constants"
 const connectDb = async () => {
     try {
        const connectionInstance =  await mongoose.connect(`${process.env.MONGODBURI}/${DB_NAME}`)
-       console.log(`MongoDb connected !!! ${connectionInstance.connection,HOST}`)
+       console.log(`MongoDb connected !!! ${connectionInstance.connection.HOST}`)
         
     } catch (error) {
         console.log(`MongoDb connection Failed :  ${error}`)

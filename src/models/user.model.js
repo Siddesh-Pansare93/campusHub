3f63ec59fe189d29aa98f942652465926ede2b3d
Siddesh-Pansare93 <siddeshpansare207@gmail.com>
2024-03-23 20:19:41 +0530

    added methods on UserSchema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose , {Schema} from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
 
 const userSchema  = new Schema({
     name : {
@@ -36,5 +38,46 @@ const userSchema  = new Schema({
         
     }
 } ,{timestamps:true})
+
+
+userSchema.pre("save" , async function(next){
+    if(!this.ismodified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password , 10)
+    next()
+})
+
+userSchema.methods.idPasswordCorrect = async function(){
+    return bcrypt.compare(password , this.password)
+}
+
+// generating jwt ( access and refresh ) Tokens 
+
+userSchema.methods.generateAccessTokens = function(){
+    jwt.sign(
+        {
+            id:this._id , 
+            username : this.username ,
+            email  : this.email
+        } ,
+        process.env.ACCESS_TOKEN_SECRET , 
+        {
+           expiresIn :  process.env.ACCESS_TOKEN_EXPIRY
+        }
+)
+}
+userSchema.methods.generateRefreshTokens = function(){
+    jwt.sign(
+        {
+            id:this._id , 
+            
+        } ,
+        process.env.REFRESH_TOKEN_SECRET , 
+        {
+           expiresIn :  process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+
 
 export const User = mongoose.model("User" , userSchema)
